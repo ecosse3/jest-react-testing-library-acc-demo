fc33d742fd8c807e4140aeaa72b75b8dd56028b7
Kurpiewski, Lukasz <lukasz.kurpiewski@accenture.com>
2021-10-12 16:39:34 +0200

    test(Counter): change plus/minus button to getByRole and name

--- src/components/Counter/index.test.tsx
+++ src/components/Counter/index.test.tsx
@@ -18,15 +18,23 @@ test('render counter with 0 initial value', () => {
 test('press Plus button once', () => {
   render(<Counter />)
   const counter = screen.getByTestId('count');
-  const plusButton = screen.getByText(/Plus/);
+  // const plusButton = screen.getByText(/Plus/i);
+  const plusButton = screen.getByRole('button', {
+    name: /Plus/i
+  })
   fireEvent.click(plusButton);
+
   expect(counter).toHaveTextContent('1');
 })
 
 test('press Minus button once', () => {
   render(<Counter />)
   const counter = screen.getByTestId('count');
-  const minusButton = screen.getByText(/Minus/);
+  // const minusButton = screen.getByText(/Minus/i);
+  const minusButton = screen.getByRole('button', {
+    name: /Minus/i
+  })
   fireEvent.click(minusButton);
+
   expect(counter).toHaveTextContent('-1');
 })
